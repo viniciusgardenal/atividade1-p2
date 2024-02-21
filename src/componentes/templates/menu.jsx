6dac03da2a11474ef20b54b58404162d1088081a
Vinicius Gardenal <viniciusgardenal@outlook.com>
2024-02-21 20:34:42 -0300

    Version 0.0.5

--- src/componentes/templates/menu.jsx
+++ src/componentes/templates/menu.jsx
@@ -5,7 +5,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Menu(props) {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className="bg-body-secondary">
       <Container>
         <Navbar.Brand className='' href="#home">Cadastramento</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
